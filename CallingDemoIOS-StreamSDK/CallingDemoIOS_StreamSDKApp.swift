380d192a081ca8c5b2b210c6a523e0a5373c461f
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-09-17 10:28:09 +0700

    Initial Commit

---
+++ CallingDemoIOS-StreamSDK/CallingDemoIOS_StreamSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallingDemoIOS_StreamSDKApp.swift
+//  CallingDemoIOS-StreamSDK
+//
+//  Created by Đoàn Văn Khoan on 17/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CallingDemoIOS_StreamSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
