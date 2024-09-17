5543747b3e117b6a4bc4d5b1bd93158384165915
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-09-17 11:34:50 +0700

    first commit

--- CallingDemoIOS-StreamSDK/CallingDemoIOS_StreamSDKApp.swift
+++ CallingDemoIOS-StreamSDK/CallingDemoIOS_StreamSDKApp.swift
@@ -6,12 +6,76 @@
 //
 
 import SwiftUI
+import StreamVideo
+import StreamVideoSwiftUI
 
 @main
-struct CallingDemoIOS_StreamSDKApp: App {
+struct VideoCallApp: App {
+    @State var call: Call
+    @ObservedObject var state: CallState
+    @State var callCreated: Bool = false
+
+    private var client: StreamVideo
+
+    private let apiKey: String = "mmhfdzb5evj2"
+    private let token: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0F1cnJhX1NpbmciLCJ1c2VyX2lkIjoiQXVycmFfU2luZyIsInZhbGlkaXR5X2luX3NlY29uZHMiOjYwNDgwMCwiaWF0IjoxNzI2NTQ1MzU4LCJleHAiOjE3MjcxNTAxNTh9.ITKyyLJYBTgUtVmylYbmA1kxGT9SipsRDIZZ1wGvy4Q"
+    private let userId: String = "Aurra_Sing"
+    private let callId: String = "lgUGZK5ldPOT"
+
+    init() {
+        let user = User(
+            id: userId,
+            name: "Martin", // name and imageURL are used in the UI
+            imageURL: .init(string: "https://getstream.io/static/2796a305dd07651fcceb4721a94f4505/a3911/martin-mitrevski.webp")
+        )
+
+        // Initialize Stream Video client
+        self.client = StreamVideo(
+            apiKey: apiKey,
+            user: user,
+            token: .init(stringLiteral: token)
+        )
+
+        // Initialize the call object
+        let call = client.call(callType: "default", callId: callId)
+
+        self.call = call
+        self.state = call.state
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            VStack {
+                if callCreated {
+                    ZStack {
+                        ParticipantsView(
+                            call: call,
+                            participants: call.state.remoteParticipants,
+                            onChangeTrackVisibility: changeTrackVisibility(_:isVisible:)
+                        )
+                        FloatingParticipantView(participant: call.state.localParticipant)
+                    }
+                } else {
+                    Text("loading...")
+                }
+            }.onAppear {
+                Task {
+                    guard callCreated == false else { return }
+                    try await call.join(create: true)
+                    callCreated = true
+                }
+            }
+        }
+    }
+    
+    /// Changes the track visibility for a participant (not visible if they go off-screen).
+    /// - Parameters:
+    ///  - participant: the participant whose track visibility would be changed.
+    ///  - isVisible: whether the track should be visible.
+    private func changeTrackVisibility(_ participant: CallParticipant?, isVisible: Bool) {
+        guard let participant else { return }
+        Task {
+            await call.changeTrackVisibility(for: participant, isVisible: isVisible)
         }
     }
 }
